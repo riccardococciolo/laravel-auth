064d3c0c8979981c2ad7fe91c6ae113fe7ecdad3
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-01-24 16:27:19 +0100

    add: modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,27 @@ import.meta.glob([
     '../img/**'
 ]);
 
+const buttons = document.querySelectorAll('.btn-delete');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+
+        event.preventDefault();
+
+
+        const deleteModal = new bootstrap.Modal('#delete-modal');
+
+
+        const title = button.getAttribute('data-title');
+        document.getElementById('title-to-delete').innerHTML = title;
+
+
+        document
+            .getElementById('action-delete')
+            .addEventListener('click', () => {
+                button.parentElement.submit();
+            })
+
+        deleteModal.show();
+    });
+})
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -48,5 +48,22 @@
                 @endforeach
             </tbody>
         </table>
+        <div class="modal" tabindex="-1" id="delete-modal">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Cancella progetto</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Vuoi veramente cancellare il progetto: <span id="title-to-delete"></span></p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                  <button type="button" id="action-delete" class="btn btn-danger">Sì, voglio cancellarlo</button>
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
 @endsection
